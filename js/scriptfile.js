1e2ea9f55402cbc4db66555fe960035f5cec9802
Nahidul Islam <nahidulislam@Nahiduls-MBP.home>
2017-12-17 22:41:13 +0000

    made changes to nav html

--- js/scriptfile.js
+++ js/scriptfile.js
@@ -1,10 +1,8 @@
+function collapsenav() { /*this function allows for the icon to collapse the nav for smaller devices*/
 
-function collapsenav() {                                    /*this function allows for the icon to collapse the nav for smaller devices*/
-    
     var changenav = document.getElementById("mainnav");
     if (changenav.className === "mainnav") {
         changenav.className += " responsivenav";
-
 
 
     } else {
@@ -18,13 +16,12 @@ function collapsenav() {                                    /*this function allo
 
 
 function stickyscroll() {
-    var navbar = document.getElementById("mainnav");                        /*this function allows for the navigation bar to stick to the top of the window*/
-    var currentpage = document.getElementById("navicon");
-    var sticky = navbar.offsetTop;                              
+    var navbar = document.getElementById("mainnav"); /*this function allows for the navigation bar to stick to the top of the window*/
+    var sticky = navbar.offsetTop;
     const slides = document.getElementsByClassName("banner");
 
     if (window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky");                                     /*if statment with a condition that checks if the user has scrolled*/
+        navbar.classList.add("sticky"); /*if statment with a condition that checks if the user has scrolled*/
 
     } else {
         navbar.classList.remove("sticky");
@@ -36,61 +33,96 @@ function stickyscroll() {
 window.onload = function () {
     document.addEventListener("scroll", stickyscroll);
     document.getElementById("navicon").addEventListener("click", collapsenav); /*when the icon is clicked the navigation collapses*/
-    responsiveSlider()
+    responsiveSlider();
 }
 
 function responsiveSlider() {
 
-  var slider = document.getElementById("homeimageslider");
-  var sliderWidth = slider.offsetWidth;
-  var slideList = document.getElementById("slidecontent");
-  var count = 1;
-  var items = slideList.querySelectorAll("li").length;
-  var prev = document.getElementById("prev");
-  var next = document.getElementById("next");
+    var slider = document.getElementById("homeimageslider");
+    var sliderWidth = slider.offsetWidth;
+    var slideList = document.getElementById("slidecontent");
+    var count = 1;
+    var items = slideList.querySelectorAll("li").length;
+    var prev = document.getElementById("prev");
+    var next = document.getElementById("next");
 
-  window.addEventListener('resize', function() {
-    sliderWidth = slider.offsetWidth;
-  });
+    window.addEventListener('resize', function () {
+        sliderWidth = slider.offsetWidth;
+    });
 
-  var prevSlide = function() {
-    if(count > 1) {
-      count = count - 2;
-      slideList.style.left = "-" + count * sliderWidth + "px";
-      count++;
-    }
-    else if(count = 1) {
-      count = items - 1;
-      slideList.style.left = "-" + count * sliderWidth  + "px";
-      count++;
-    }
-  };
+    var prevSlide = function () {
+        if (count > 1) {
+            count = count - 2;
+            slideList.style.left = "-" + count * sliderWidth + "px";
+            count++;
+        } else if (count = 1) {
+            count = items - 1;
+            slideList.style.left = "-" + count * sliderWidth + "px";
+            count++;
+        }
+    };
 
-  var nextSlide = function() {
-    if(count < items) {
-      slideList.style.left = "-" + count * sliderWidth  + "px";
-      count++;
-    }
-    else if(count = items) {
-      slideList.style.left = "0px";
-      count = 1;
-    }
-  };
-  
-  next.addEventListener("click", function() {
-    nextSlide();
-  });
+    var nextSlide = function () {
+        if (count < items) {
+            slideList.style.left = "-" + count * sliderWidth + "px";
+            count++;
+        } else if (count = items) {
+            slideList.style.left = "0px";
+            count = 1;
+        }
+    };
 
-  prev.addEventListener("click", function() {
-    prevSlide();
-  });
-  
-  setInterval(function() {
-    nextSlide()
-  }, 8000);
+    next.addEventListener("click", function () {
+        nextSlide();
+    });
+
+    prev.addEventListener("click", function () {
+        prevSlide();
+    });
+
+    setInterval(function () {
+        nextSlide()
+    }, 4000);
 
 };
 
+var targetOffset, currentPosition,
+    body = document.body,
+    button = document.getElementById('scrollButton'),
+    animateTime = 900;
+
+function getPageScroll() {
+  var yScroll;
+
+  if (window.pageYOffset) {
+    yScroll = window.pageYOffset;
+  } else if (document.documentElement && document.documentElement.scrollTop) {
+    yScroll = document.documentElement.scrollTop;
+  } else if (document.body) {
+    yScroll = document.body.scrollTop;
+  }
+  return yScroll;
+}
+
+button.addEventListener('click', function (event) {
+
+  targetOffset = document.getElementById(event.target.hash.substr(1)).offsetTop;
+  currentPosition = getPageScroll();
+
+  body.classList.add('in-transition');
+  body.style.WebkitTransform = "translate(0, -" + (targetOffset - currentPosition) + "px)";
+  body.style.MozTransform = "translate(0, -" + (targetOffset - currentPosition) + "px)";
+  body.style.transform = "translate(0, -" + (targetOffset - currentPosition) + "px)";
+
+  window.setTimeout(function () {
+    body.classList.remove('in-transition');
+    body.style.cssText = "";
+    window.scrollTo(0, targetOffset);
+  }, animateTime);
+
+  event.preventDefault();
+
+}, false);
 
 
 
